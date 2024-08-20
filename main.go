3ec2e92dc60b7b24253677d636c572699e6f2152
ImValerio <valerio.valletta@icloud.com>
2024-08-20 20:41:28 +0200

    add port on env var

--- main.go
+++ main.go
@@ -133,7 +133,17 @@ func main() {
 		w.Write([]byte("hi"))
 	})
 
-	http.ListenAndServe(":8080", r)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("defaulting to port %s", port)
+	}
+
+	// Start HTTP server.
+	log.Printf("listening on port %s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
 }
 
 func IsBinaryString(s string) bool {
